cfef7f578180de51df34c9cb2e50615973e5fedc
Tucker Reiland <tuckerreiland@Tuckers-MacBook-Pro.local>
2024-01-24 12:14:34 -0800

    created switch for updating slugs and their relations

--- app/api/blog/collection/route.js
+++ app/api/blog/collection/route.js
@@ -1,17 +1,29 @@
-export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(req) {
+export const dynamic = 'force-dynamic'
+
+import { toKebabCase } from "@/lib/utils"
+import { db } from "@/prisma"
+
+export async function GET() {
     try {
-        const data = await res.json()
-        return Response.json({data})
+        const res = await db.blogCollection.findMany()
+        return Response.json(res)
     } catch (error) {
         return Response.json({error})
     }
 }
+
 export async function POST(req) {
     try {
-        const data = await res.json()
-        return Response.json({data})
+        const data = await req.json()
+        const res = await db.blogCollection.create({
+            data:{
+                slug: toKebabCase(data.title),
+                ...data
+            }
+        })
+        return Response.json(res)
     } catch (error) {
+        console.log(error)
         return Response.json({error})
     }
 }
--- app/api/blog/post/route.js
+++ app/api/blog/post/route.js
@@ -1,5 +1,6 @@
 export const dynamic = 'force-dynamic'
 
+import { toKebabCase } from "@/lib/utils"
 import { db } from "@/prisma"
 
 export async function GET() {
@@ -15,7 +16,15 @@ export async function POST(req) {
     try {
         const data = await req.json()
         const res = await db.post.create({
-            data:data
+            data:{
+                ...data,
+                slug: toKebabCase(data.title),
+                collections: {
+                    connect: data.collections.map((collection)=> {
+                        return {slug: collection}
+                    })
+                }
+            }
         })
         return Response.json(res)
     } catch (error) {
